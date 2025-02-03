c5405e65c73425346c5148f61eb93b2cc264b956
talha <atercan0658@gmail.com>
2025-02-03 12:22:39 +0100

    setup express and mongoDB app

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+async function main() {
+  const dbURI = process.env.DB_URI;
+
+  if (!dbURI) {
+    console.error(
+      "Database URI is missing. Please set DB_URI in your .env file."
+    );
+    process.exit(1);
+  }
+
+  await mongoose.connect(dbURI);
+  app.listen(8080);
+}
+
+try {
+  main();
+} catch (error) {
+  console.log(error);
+}
+
+router.get("/", (req, res) => res.send("home"));
